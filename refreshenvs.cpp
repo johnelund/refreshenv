ac0b36b6474f111b8b4c4ee13eacb910f312a878
John Lund <jel@appsmiths.com>
2018-12-06 15:53:11 -0600

    First commit

---
+++ refreshenvs.cpp
@@ -0,0 +1,12 @@
+﻿// refreshenvs.cpp
+// Refresh the Windows environment variables.
+// A new shell is still required, but no reboot.
+// I use this when switching MSYS environments...
+//
+
+#include <Windows.h>
+
+int main()
+{
+    SendMessage(NULL, WM_SETTINGCHANGE, NULL, (LPARAM) "Environment");
+}
